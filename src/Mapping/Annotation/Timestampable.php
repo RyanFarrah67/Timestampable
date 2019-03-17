9596dc98a38833d1cdd1e6ec0ce6693a0510c241
Ryan <ryanfarrah25410@gmail.com>
2019-03-17 23:49:27 +0100

    composer.json : add library informations create Timestampable class which is the Doctrine annotation

---
+++ src/Mapping/Annotation/Timestampable.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Mof\Mapping\Annotation;
+
+use Doctrine\Common\Annotations\Annotation;
+use Doctrine\Common\Annotations\Annotation\Target;
+
+/**
+ * @Annotation
+ * @Target("PROPERTY")
+ */
+class Timestampable
+{
+    public $createdAt;
+
+    public $updatedAt;
+}
